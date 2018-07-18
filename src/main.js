02f1bf4d4262454a210db168fe4a671b9b9ac3b3
ghigbie <gehigbie@gmail.com>
2018-07-17 18:53:41 -0600

    Add v-bind syntax

--- src/main.js
+++ src/main.js
@@ -1,25 +1,45 @@
 import Vue from 'vue';
 import './style.scss';
 
+import genres from './util/genres';
+
 new Vue({
     el: '#app',
     components: {
         'movie-list': {
             template: `<div id="movie-list">
-                           <div v-for"movie in movies">{{ movie.title }}</div>
+                           <div v-for="movie in movies"
+                                class="movie">{{ movie.title }}</div>
                        </div>`,
-            data: {
-                movies: [
-                    { title: 'Pulp Fiction' },
-                    { title: 'Home Alone' },
-                    { title: 'Austin Powers' }
-                ]
+            data(){ //must be a function
+                return {
+                    movies: [
+                        { title: 'Pulp Fiction' },
+                        { title: 'Home Alone' },
+                        { title: 'Austin Powers' }
+                    ]
+                }
             }
         },
         'movie-filter': {
+            data() {
+                return {
+                    genres //genres is imported from above. 
+                }
+            },
             template: `<div id="movie-filter">
-                           Movie Filter
-                       </div>`
-        }
+                           <h2>Filter Results</h2>
+                           <div class="filter-group">
+                                <check-filter v-for="genre in genres"
+                                              :title="genre"></check-filter>
+                           </div>
+                       </div>`,
+            components: {
+                'check-filter': {
+                    props: ['title'],
+                    template: `<div>{{ title }}</div>`
+                }
+            }
+        }, 
     }
 });
